f762c74184f711b407a337b854ce2f38233ba4c1
Entebengel <chris.ende@gmx.de>
2025-06-19 18:39:54 +0200

    livery klappt nicht

--- src/pages/LiveryListe.tsx
+++ src/pages/LiveryListe.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LiveryListe = () => (
     <div>
-        <h1>Livery Liste</h1>
+        <h1>Hier gibt es Liverys zu kaufen!</h1>
         {/* Hier können später die einzelnen Livery-Artikel gelistet werden */}
         <p>Hier findest du alle verfügbaren Livery-Designs.</p>
     </div>
