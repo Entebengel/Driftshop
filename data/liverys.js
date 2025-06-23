8e4f3405fb51be83ab5043541dbd5badb1c7beb3
Entebengel <chris.ende@gmx.de>
2025-06-23 13:12:22 +0200

    liste

---
+++ data/liverys.js
@@ -0,0 +1,19 @@
+export const liverys = [
+  {
+    id: "livery1",
+    name: "Racing Stripe",
+    shortDesc: "Klassische Rennstreifen für sportlichen Look.",
+    longDesc: "Verleihe deinem Fahrzeug einen sportlichen Look mit klassischen Rennstreifen. Hochwertige Folie, UV-beständig und langlebig.",
+    price: 199,
+    images: ["images/liverys_kategorie.jpg", "images/liverys_kategorie.jpg"]
+  },
+  {
+    id: "livery2",
+    name: "Urban Camo",
+    shortDesc: "Modernes Camouflage-Design für Individualisten.",
+    longDesc: "Das Urban Camo Design hebt dein Auto von der Masse ab. Perfekt für alle, die auffallen wollen.",
+    price: 249,
+    images: ["images/liverys_kategorie.jpg", "images/liverys_kategorie.jpg"]
+  },
+  // ...weitere Artikel...
+];
